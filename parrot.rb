c0d495477b02aa831de8a5aee278f804fdac07f4
Peggy Lee <peggyleesix@gmail.com>
2018-06-20 17:59:40 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(noise=Squawk!)
+def parrot
+  noise = Squawk!
+end
+
+def parrot_call
   noise
+  puts noise
 end
